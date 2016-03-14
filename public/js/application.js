382199f25b354e3cb56fed23e5a0ba7bff2c5131
bobdorff <robertdorff@gmail.com>
2016-03-14 13:01:38 -0500

    basic routing in place

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+	$("#about").on('click', function(event) {
+		event.preventDefault();
+		var route = $(this).attr('href')
+		var request = $.ajax({
+			url: route, 
+			method: "GET",
+		});
+	});
 });
