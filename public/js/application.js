190a8aad4f026f294fb61d57352dc2f9e4061918
bobdorff <robertdorff@gmail.com>
2016-03-14 13:04:23 -0500

    add ajax hooks

--- public/js/application.js
+++ public/js/application.js
@@ -7,4 +7,14 @@ $(document).ready(function() {
 			method: "GET",
 		});
 	});
+
+	$("#projects").on('click', function(event) {
+		event.preventDefault();
+		var route = $(this).attr('href')
+		var request = $.ajax({
+			url: route, 
+			method: "GET",
+		});
+	});
+	
 });
